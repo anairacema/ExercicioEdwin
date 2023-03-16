50a2002eade735418a239da5582c747d1b73750e
Ana Iracema <nana.iracema@gmail.com>
2023-03-16 20:20:23 -0300

    Caixinha de numero que é apresentado em ela e depois no console

---
+++ Edwin/src/Aula1603.java
@@ -0,0 +1,19 @@
+
+import javax.swing.*;
+
+
+public class Aula1603 {
+	
+	public static void main(String[] args) {
+		int num1 = 0;
+		String mens = "";
+		num1 = Integer.parseInt(JOptionPane.showInputDialog("Digite a nota: ", 6));
+		
+		System.out.println("O valor é:" + num1);
+	}
+	
+
+}
+
+
+
