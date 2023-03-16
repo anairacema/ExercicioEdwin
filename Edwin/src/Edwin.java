372956a06532f5554734a108f5e8119fbdbd7f4f
Ana Iracema <nana.iracema@gmail.com>
2023-03-16 18:42:22 -0300

    Lista de exercicios executados com Edwin

---
+++ Edwin/src/Edwin.java
@@ -0,0 +1,17 @@
+
+public class Edwin {
+
+	public static void main(String[] args) {
+		double nota ;
+		System.out.println("Digite uma nota: ");
+		nota = 60;
+		if (nota >= 70) {
+			System.out.println("Aprovado");
+		}
+		else {
+			System.out.println("Reprovado");
+		}
+
+	}
+
+}
